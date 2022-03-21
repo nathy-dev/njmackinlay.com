7f0653cc6e55fd5d4653eb87f225e5cfa125ea0d
nathy <nmackinl@asu.edu>
2022-03-21 13:15:22 -0700

    updated footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Footer.css'
+import {BsGithub, BsLinkedin} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <h5>Made with 💙 by njmackinlay</h5>
+      <a href='https://github.com/njmackinlay' target="_blank" rel='noreferrer'><BsGithub/></a>
+      <a href='https://linkedin.com/in/njmackinlay' target="_blank" rel='noreferrer'><BsLinkedin/></a>
+    </div>
   )
 }
 
