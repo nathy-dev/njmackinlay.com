288f426c8538ecbf3ba0e25852fc57b2f99feaee
nathy <nmackinl@asu.edu>
2022-03-21 23:15:49 -0700

    updated projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
 import './Portfolio.css'
-import banan from '../../assets/banan.jpeg'
+import hotdog from '../../assets/hotdog.jpeg'
+import cc from '../../assets/crash-course.png'
+import robo from '../../assets/robot.jpeg'
+import rc from '../../assets/react-craft.png'
 
 const data = [
   {
     id: 1,
-    image: banan,
+    image: hotdog,
     title: 'Hot Dog or Not Hot Dog',
-    small: 'A simple application that uses google api to classify image',
-    git: 'https://github.com'
+    small: 'React Native, GoogleCloud, CloudVision',
+    smallContinued: "Inspired by HBO's Silicon Valley, this app classifies an image to determine whether it is a hotdog",
+    git: 'https://github.com/njmackinlay/hotdog-or-not-hotdog'
   },
   {
     id: 2,
-    image: banan,
-    title: 'Hot Dog or Not Hot Dog',
-    small: 'A simple application that uses google api to classify image',
-    git: 'https://github.com'
-  }
+    image: rc,
+    title: 'React MineCraft',
+    small: 'Reactjs, ThreeJs, WebGl',
+    smallContinued: "A playable, simplified version of the game minecraft that supports crafting, various bricks, and general gameplay",
+    git: 'https://github.com/njmackinlay/react-cubed'
+  },
+  {
+    id: 3,
+    image: robo,
+    title: 'Warehouse Automation',
+    small: 'Clingo, Classic ASP',
+    smallContinued: "A solution to the 2019 ASP challenge that seeks to discover optimal routes for stocking and transport automation",
+    git: 'https://github.com/njmackinlay/Warehouse-Automation'
+  },
+  {
+    id: 4,
+    image: cc,
+    title: 'Crash Course Web Game',
+    small: 'Vanilla Js, HTML, CSS',
+    smallContinued: "An html game that teaches players about insurance. Placed 1st for creativity and 3rd overall in the StateFarm 2020 Annual Hackathon",
+    git: 'https://github.com/njmackinlay/Crash-Course'
+  },
 ]
 
 const Portfolio = () => {
@@ -27,7 +48,7 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, small, git}) => {
+          data.map(({id, image, title, small, smallContinued, git}) => {
             return (
               <article key={id} className='portfolio__item'>
               <div className='portfolio__item-image'>          
@@ -35,6 +56,7 @@ const Portfolio = () => {
               </div>
                 <h3>{title}</h3>
                 <small><i>{small}</i></small><br/>
+                <small>{smallContinued}</small><br/>
                 <a className='btn btn-primary' href={git} target='_blank' rel='noreferrer'>View Code</a>
             </article>
             )
